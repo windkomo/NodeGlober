e23a247145dc709f91312349397e135f412938d7
Komo <windkomo@gmail.com>
2014-12-26 11:44:54 +0100

    query parameter added for mog

--- routes/mog.js
+++ routes/mog.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var MogController = rekuire('mogController');
 
 /* GET top mogs listing. */
-router.get('/topmogs/:language', function(req, res) {
+router.get('/topmogs', function(req, res) {
 
-    var languageId = req.params.language;
+    var languageId = req.param('languageId');
     var mogController = new MogController();
     mogController.getMogs(languageId, function(err, result) {
         if (err) throw err;
